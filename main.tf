62b55f239183227a0e3c9f86ee52ca0761aefac3
Gagandeep Singh <admin.gagan@gmail.com>
2021-11-26 15:47:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "server2" {
   ami           = "ami-0ba62214afa52bec7"
   instance_type = "t2.medium"
   tags = {
-    Name = "gds-terraform-v2"
+    Name = "gds-terraform-v3"
     Env  = "Prod"
   }
 }
